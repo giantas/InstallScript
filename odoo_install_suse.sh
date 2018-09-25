8385f2281735eda14683efc21f322dce55cea132
Aswa Paul <aswapaul@gmail.com>
2018-09-25 17:28:17 +0300

    Fix: extra argument

--- odoo_install_suse.sh
+++ odoo_install_suse.sh
@@ -202,7 +202,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-sudo git clone --depth 1 --branch --single-branch $OE_VERSION https://www.github.com/odoo/odoo "$OE_HOME_EXT/"
+sudo git clone --depth 1 --branch $OE_VERSION --single-branch https://www.github.com/odoo/odoo "$OE_HOME_EXT/"
 
 if [ "$IS_ENTERPRISE" = true ]; then
     # Odoo Enterprise install!
